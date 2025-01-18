23af767e7edfaf18105071bb0f02d53b209db7c9
Falka <valentyna.stashenko@gmail.com>
2025-01-18 21:30:22 +0200

    hw-07 v.1.2

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,5 @@
 const spanElem = document.querySelector("span#name-output");
+const inputElem = document.querySelector("input#name-input");
 const inputHandler = () => {
   let value = inputElem.value.trim();
   if (value === "") {
